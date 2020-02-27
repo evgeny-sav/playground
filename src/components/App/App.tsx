ae343a08739698677f9a9d91cab39d595bc5dd55
Yauheni Sauchanka <ysauchanka@travix.com>
2020-02-27 12:00:49 +0300

    changes

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import logo from '../../assets/logo.svg';
+import './App.scss';
+import { AppProps } from './types';
+
+const App: FC<AppProps> = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+};
+
+export default App;
